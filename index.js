77885e50fe6c36e55ae9c49f603c6d8cc0733657
Chris Zubak-Skees <chriszs@gmail.com>
2016-10-19 17:35:43 -0400

    Standardize index.js style

--- index.js
+++ index.js
@@ -3,30 +3,28 @@ var parse = require('url-parse');
 var streamer = {};
 
 streamer.getInserter = function(config) {
-  // return a new inserter class
-  var parsed;
-  if(config.useSequelizeBulkInsert) {
-    return require('./lib/inserters/sequelizeBulkInserter.js')(config);
-  } else if(config.dbConnString) {
-    parsed = parse(config.dbConnString);
-  }
-  switch(parsed.protocol) {
-    case 'postgres:':
-      return require('./lib/inserters/pgInserter.js')(config);
-      break;
-    case 'mysql:':
-      if(parsed) {
-        config.dbname = parsed.pathname.substr(1);
-        config.username = parsed.username;
-        config.password = parsed.password;
-        config.hostname = parsed.hostname;
-        config.port = parseInt(parsed.port, 10);
-      }
-      return require('./lib/inserters/mySqlInserter.js')(config);
-    default:
-      return require('./lib/inserters/sequelizeBulkInserter.js')(config);
-      break;
-  }
+    // return a new inserter class
+    var parsed;
+    if (config.useSequelizeBulkInsert) {
+        return require('./lib/inserters/sequelizeBulkInserter.js')(config);
+    } else if (config.dbConnString) {
+        parsed = parse(config.dbConnString);
+    }
+    switch (parsed.protocol) {
+        case 'postgres:':
+            return require('./lib/inserters/pgInserter.js')(config);
+        case 'mysql:':
+            if (parsed) {
+                config.dbname = parsed.pathname.substr(1);
+                config.username = parsed.username;
+                config.password = parsed.password;
+                config.hostname = parsed.hostname;
+                config.port = parseInt(parsed.port, 10);
+            }
+            return require('./lib/inserters/mySqlInserter.js')(config);
+        default:
+            return require('./lib/inserters/sequelizeBulkInserter.js')(config);
+    }
 };
 
 module.exports = streamer;
