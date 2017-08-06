673f17fbeada7f41ce0dcc04a0dd40d2481d7546
Chris Zubak-Skees <chriszs@gmail.com>
2017-08-06 19:54:42 -0400

    Ahaha, it works!

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var parse = require('url-parse');
 
 module.exports = config => {
     // return a new inserter class
+    /*
     let parsed;
     
     if (config.useSequelizeBulkInsert) {
@@ -11,10 +12,10 @@ module.exports = config => {
     }
     else if (config.dbConnString) {
         parsed = parse(config.dbConnString);
-    }
+    }*/
 
-    switch (parsed.protocol) {
-        case 'postgres:':
+    switch (config.sequelize.options.dialect) {
+        case 'postgres':
             return require('./lib/inserters/pgInserter.js')(config);
         default:
             return require('./lib/inserters/sequelizeBulkInserter.js')(config);
